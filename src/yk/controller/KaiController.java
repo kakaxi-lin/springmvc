f3e850987251d25982764e677229938966116117
kiki <740591134@qq.com>
2017-03-14 17:22:08 +0800

    update

--- src/yk/controller/KaiController.java
+++ src/yk/controller/KaiController.java
@@ -1,19 +1,28 @@
 package yk.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-@Controller("/k")
+
+import yk.service.KaiService;
+@Controller
 @RequestMapping("/a")
 public class KaiController {
+	@Resource
+	private KaiService ks;
+	public void setKs(KaiService ks) {
+		this.ks = ks;
+	}
 	@RequestMapping("/zj")
 	public ModelAndView handleRequest(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("enter1...");
+		ks.marryWho();
 		return new ModelAndView("../k.html");
 	}
 	@RequestMapping("/k")
---
+++ src/yk/service/KaiService.java
@@ -0,0 +1,11 @@
+package yk.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KaiService {
+	public void marryWho(){
+		System.out.println("Äãµ½µ×ÊÇË­¡£¡£");
+	}
+
+}
