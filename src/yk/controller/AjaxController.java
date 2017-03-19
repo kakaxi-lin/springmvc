7cba2434d32cf7b2f4ab7570c2df267035371397
kk <kk@qq.com>
2017-03-19 13:01:23 +0800

    xin sui

--- src/yk/controller/AjaxController.java
+++ src/yk/controller/AjaxController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import yk.po.Who;
 
 @Controller
@@ -20,5 +21,6 @@ public class AjaxController {
 			list.add(new Who("2","¿¨¿¨Î÷"));
 			return list;
 		}
+		
 
 }
